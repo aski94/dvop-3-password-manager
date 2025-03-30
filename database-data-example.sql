33aaa010831f821d6f6e2cf20a652f44814a77c3
Aleš Aubrecht <alesaubr@gmail.com>
2025-03-30 12:18:16 +0200

    chore: updated database records

--- database-data-example.sql
+++ database-data-example.sql
@@ -1,21 +1,21 @@
-INSERT INTO "user" ("user_id", "username", "password") VALUES
-                                                           (1, 'ales', 'Heslo123'),
-                                                           (2, 'pepa', 'heslicko');
+INSERT INTO "user" ("username", "password") VALUES
+                                                ('ales', 'Heslo123'),
+                                                ('mysak', 'tykraso');
 
-INSERT INTO "group" ("group_id", "name") VALUES
-                                             (1, 'pristupy spolecne'),
-                                             (2, 'pristupy ales');
+INSERT INTO "group" ("name") VALUES
+                                 ('pristupy spolecne'),
+                                 ('pristupy ales');
 
 INSERT INTO "group_user" ("group_id", "user_id") VALUES
-                                                     (1, 1), (1, 2), -- 'pristupy spolecne' contains users 1 and 2
-                                                     (2, 1);         -- 'pristupy ales' contains user 1
+                                                     (1, 1), (1, 2),
+                                                     (2, 1);
 
-INSERT INTO "password" ("password_id", "description", "group_id", "username", "password") VALUES
-                                                                                              (1, 'pristup na seznam.cz', 2, 'alesau', 'Heslo123'),
-                                                                                              (2, 'pristup skola', 1, 'user', 'Pass'),
-                                                                                              (3, 'pristup skola2', 1, 'user2', 'Pass2'),
-                                                                                              (4, 'pristup skola3', 1, 'user3', 'Pass3');
+INSERT INTO "password" ("description", "group_id", "username", "password") VALUES
+                                                                               ('pristup na seznam.cz', 2, 'alesau', 'Heslo123'),
+                                                                               ('pristup skola', 1, 'user', 'Pass'),
+                                                                               ('pristup skola2', 1, 'user2', 'Pass2'),
+                                                                               ('pristup skola3', 1, 'user3', 'Pass3');
 
-INSERT INTO "log" ("log_id", "date", "group_id", "description") VALUES
-                                                                    (1, '2025-03-01 10:00:00', 1, 'Added password to group "pristupy spolecne"'),
-                                                                    (2, '2025-03-01 10:00:01', 1, 'Added password to group "pristupy spolecne"');
+INSERT INTO "log" ("date", "group_id", "description") VALUES
+                                                          ('2025-03-01 10:00:00', 1, 'Added password Id 1 to group "pristupy spolecne"'),
+                                                          ('2025-03-01 10:00:01', 1, 'Added password Id 2 to group "pristupy spolecne"');
