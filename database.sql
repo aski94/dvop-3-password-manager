293e14099aaf4197f87066ba2ab9072941f7e355
Aleš Aubrecht <alesaubr@gmail.com>
2025-03-30 11:43:53 +0200

    chore: updated database records

--- database.sql
+++ database.sql
@@ -1,31 +1,31 @@
-CREATE TABLE user (
-                      user_id SERIAL PRIMARY KEY,
-                      username TEXT NOT NULL UNIQUE,
-                      password TEXT NOT NULL
+CREATE TABLE "user" (
+                        "user_id" SERIAL PRIMARY KEY,
+                        "username" TEXT NOT NULL UNIQUE,
+                        "password" TEXT NOT NULL
 );
 
-CREATE TABLE group (
-                       group_id SERIAL PRIMARY KEY,
-                       name TEXT NOT NULL UNIQUE
+CREATE TABLE "group" (
+                         "group_id" SERIAL PRIMARY KEY,
+                         "name" TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE group_user (
-                            group_id INT REFERENCES group(group_id) ON DELETE CASCADE,
-                            user_id INT REFERENCES user(user_id) ON DELETE CASCADE,
-                            PRIMARY KEY (group_id, user_id)
+CREATE TABLE "group_user" (
+                              "group_id" INT REFERENCES "group"("group_id") ON DELETE CASCADE,
+                              "user_id" INT REFERENCES "user"("user_id") ON DELETE CASCADE,
+                              PRIMARY KEY ("group_id", "user_id")
 );
 
-CREATE TABLE password (
-                          password_id SERIAL PRIMARY KEY,
-                          description TEXT NOT NULL,
-                          group_id INT REFERENCES group(group_id) ON DELETE CASCADE,
-                          username TEXT NOT NULL,
-                          password TEXT NOT NULL
+CREATE TABLE "password" (
+                            "password_id" SERIAL PRIMARY KEY,
+                            "description" TEXT NOT NULL,
+                            "group_id" INT REFERENCES "group"("group_id") ON DELETE CASCADE,
+                            "username" TEXT NOT NULL,
+                            "password" TEXT NOT NULL
 );
 
-CREATE TABLE log (
-                     log_id SERIAL PRIMARY KEY,
-                     date TIMESTAMP NOT NULL,
-                     user_id INT REFERENCES user(user_id) ON DELETE SET NULL,
-                     description TEXT NOT NULL
+CREATE TABLE "log" (
+                       "log_id" SERIAL PRIMARY KEY,
+                       "date" TIMESTAMP NOT NULL,
+                       "user_id" INT REFERENCES "user"("user_id") ON DELETE SET NULL,
+                       "description" TEXT NOT NULL
 );
