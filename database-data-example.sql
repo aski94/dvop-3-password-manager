2d2bbda594b65a98f8d1fc5777978f53f24d8257
Aleš Aubrecht <alesaubr@gmail.com>
2025-05-30 22:09:56 +0200

    chore: rename and translate data example

--- database-data-example.sql
+++ database-data-example.sql
@@ -1,21 +1,21 @@
 INSERT INTO "user" ("username", "password") VALUES
-                                                ('ales', 'Heslo123'),
-                                                ('pepa', 'heslicko');
+                                                ('Ales', 'Password123'),
+                                                ('Pepa', 'password');
 
 INSERT INTO "group" ("name") VALUES
-                                 ('pristupy spolecne'),
-                                 ('pristupy ales');
+                                 ('Team Vault'),
+                                 ('Ales''s Vault');
 
 INSERT INTO "group_user" ("group_id", "user_id") VALUES
                                                      (1, 1), (1, 2),
                                                      (2, 1);
 
 INSERT INTO "password" ("description", "group_id", "username", "password") VALUES
-                                                                               ('pristup na seznam.cz', 2, 'alesau', 'Heslo123'),
-                                                                               ('pristup skola', 1, 'user', 'Pass'),
-                                                                               ('pristup skola2', 1, 'user2', 'Pass2'),
-                                                                               ('pristup skola3', 1, 'user3', 'Pass3');
+                                                                               ('Access to seznam.cz', 2, 'alesau', 'Password123'),
+                                                                               ('School Access', 1, 'user', 'Pass'),
+                                                                               ('School Access 2', 1, 'user2', 'Pass2'),
+                                                                               ('School Access 3', 1, 'user3', 'Pass3');
 
 INSERT INTO "log" ("date", "group_id", "description") VALUES
-                                                          ('2025-03-01 10:00:00', 1, 'Added password Id 1 to group "pristupy spolecne"'),
-                                                          ('2025-03-01 10:00:01', 1, 'Added password Id 2 to group "pristupy spolecne"');
+                                                          ('2025-03-01 10:00:00', 1, 'Added password Id 1 to group "Team Vault"'),
+                                                          ('2025-03-01 10:00:01', 1, 'Added password Id 2 to group "Team Vault"');
